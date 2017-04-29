c0c4c673ca58805727211f659e334688b629eff0
matrixd <matrixdaniil@gmail.com>
2017-04-29 14:06:55 +0300

    init

---
+++ objectpool.hpp
@@ -0,0 +1,233 @@
+#ifndef CONNECTIONPOOL_HPP
+#define CONNECTIONPOOL_HPP
+
+#include <memory>
+#include <functional>
+#include <deque>
+#include <mutex>
+#include <atomic>
+
+template <typename Object>
+class ObjectPool
+{
+public:
+    using ObjectPtr = std::unique_ptr<Object, std::function<void(Object*)>>;
+    using ObjectFreeFunction = std::function<void(Object*)>;
+    using ObjectBackToPoolFunction = std::function<void(Object*)>;
+    using ObjectFabric = std::function<Object*()>;
+    using ObjectPoolPtr = std::shared_ptr<ObjectPool<Object>>;
+
+    explicit ObjectPool(ObjectFabric fabric, ObjectFreeFunction freeFunc, ObjectBackToPoolFunction backFunc, int startSize, int maxSize);
+    ~ObjectPool();
+    void destroy();
+
+    ObjectPtr object();
+    int count() const;
+
+private:
+    void freeObject(Object *object);
+
+    std::deque<Object*> m_objs;
+    ObjectBackToPoolFunction m_backFunc;
+    ObjectFreeFunction m_freeFunc;
+    ObjectFabric m_fabric;
+    std::mutex m_ojbsMutex;
+    int m_normalSize;
+    bool m_shouldDestroy;
+    std::atomic_int m_objsCount;
+};
+
+template <typename Object>
+class ObjectPoolBuilder
+{
+public:
+
+    ObjectPoolBuilder();
+
+    ObjectPoolBuilder &setObjectBackToPoolFunction(typename ObjectPool<Object>::ObjectBackToPoolFunction connectionBackToPoolFunc);
+    ObjectPoolBuilder &setObjectFreeFunction(typename ObjectPool<Object>::ObjectFreeFunction connectionFreeFunc);
+    ObjectPoolBuilder &setObjectFabric(typename ObjectPool<Object>::ObjectFabric connectionFabric);
+    ObjectPoolBuilder &setStartPoolSize(int size);
+    ObjectPoolBuilder &setNormalPoolSize(int size);
+
+    typename ObjectPool<Object>::ObjectPoolPtr build();
+
+private:
+    typename ObjectPool<Object>::ObjectBackToPoolFunction m_connectionBackToPoolFunc;
+    typename ObjectPool<Object>::ObjectFreeFunction m_connectionFreeFunc;
+    typename ObjectPool<Object>::ObjectFabric m_connectionFabric;
+    int m_startPoolSize;
+    int m_maxPoolSize;
+};
+
+template<typename Object>
+typename ObjectPool<Object>::ObjectPtr ObjectPool<Object>::object()
+{
+    Object *con = nullptr;
+
+    m_ojbsMutex.lock();
+    if (!m_objs.empty())
+    {
+        con = m_objs.front();
+        m_objs.pop_front();
+    }
+    m_ojbsMutex.unlock();
+
+    if (!con)
+    {
+        con = m_fabric();
+    }
+
+    m_objsCount++;
+
+    return ObjectPtr(con, [this](Object *ptr){ this->freeObject(ptr); });
+}
+
+template<typename Object>
+ObjectPool<Object>::ObjectPool(
+                                   ObjectPool::ObjectFabric fabric,
+                                   ObjectPool::ObjectFreeFunction freeFunc,
+                                   ObjectPool::ObjectBackToPoolFunction backFunc,
+                                   int startSize,
+                                   int normalSize
+                               )
+        : m_fabric(fabric)
+        , m_freeFunc(freeFunc)
+        , m_backFunc(backFunc)
+        , m_normalSize(normalSize)
+        , m_shouldDestroy(false)
+        , m_objsCount(0)
+{
+    while (startSize)
+    {
+        m_objs.push_back(m_fabric());
+        startSize--;
+    }
+}
+
+template<typename Object>
+void ObjectPool<Object>::destroy()
+{
+    m_ojbsMutex.lock();
+    m_shouldDestroy = true;
+
+    m_ojbsMutex.unlock();
+}
+
+template<typename Object>
+void ObjectPool<Object>::freeObject(Object *object)
+{
+    m_ojbsMutex.lock();
+    if (m_shouldDestroy || (m_objsCount > m_normalSize && m_normalSize > 0))
+    {
+        m_freeFunc(object);
+    }
+    else
+    {
+        m_backFunc(object);
+        m_objs.push_back(object);
+    }    
+    m_objsCount--;
+
+    if (m_shouldDestroy && m_objsCount == 0)
+    {
+        //should prevent double-free I hope
+        m_objsCount++;
+        m_ojbsMutex.unlock();
+
+        delete this;
+    }
+    m_ojbsMutex.unlock();
+}
+
+template<typename Object>
+int ObjectPool<Object>::count() const {
+    return m_objsCount;
+}
+
+
+template<typename Object>
+ObjectPool<Object>::~ObjectPool() {
+    while (!m_objs.empty())
+    {
+        m_freeFunc(m_objs.front());
+        m_objs.pop_front();
+    }
+}
+
+template<typename Object>
+ObjectPoolBuilder<Object> &ObjectPoolBuilder<Object>::setObjectBackToPoolFunction(
+        typename ObjectPool<Object>::ObjectBackToPoolFunction connectionBackToPoolFunc)
+{
+    m_connectionBackToPoolFunc = connectionBackToPoolFunc;
+    return *this;
+}
+
+template<typename Object>
+ObjectPoolBuilder<Object> &ObjectPoolBuilder<Object>::setObjectFreeFunction(
+        typename ObjectPool<Object>::ObjectFreeFunction connectionFreeFunc)
+{
+    m_connectionFreeFunc = connectionFreeFunc;
+    return *this;
+}
+
+template<typename Object>
+ObjectPoolBuilder<Object> &ObjectPoolBuilder<Object>::setObjectFabric(
+        typename ObjectPool<Object>::ObjectFabric connectionFabric)
+{
+    m_connectionFabric = connectionFabric;
+    return *this;
+}
+
+template<typename Object>
+ObjectPoolBuilder<Object> &ObjectPoolBuilder<Object>::setStartPoolSize(int size)
+{
+    m_startPoolSize = size;
+    return *this;
+}
+
+template<typename Object>
+ObjectPoolBuilder<Object> &ObjectPoolBuilder<Object>::setNormalPoolSize(int size)
+{
+    m_maxPoolSize = size;
+    return *this;
+}
+
+template<typename Object>
+ObjectPoolBuilder<Object>::ObjectPoolBuilder()
+    : m_maxPoolSize(-1)
+    , m_startPoolSize(3)
+{
+
+}
+
+template<typename Object>
+typename ObjectPool<Object>::ObjectPoolPtr ObjectPoolBuilder<Object>::build()
+{
+    if (!m_connectionFabric)
+    {
+        m_connectionFabric = [] () { return new Object(); };
+    }
+
+    if (!m_connectionBackToPoolFunc)
+    {
+        m_connectionBackToPoolFunc = [] (Object*) {};
+    }
+
+    if (!m_connectionFreeFunc)
+    {
+        m_connectionFreeFunc = [] (Object *ptr) { delete ptr; };
+    }
+
+    ObjectPool<Object> *pool = new ObjectPool<Object>(
+                    m_connectionFabric,
+                    m_connectionFreeFunc,
+                    m_connectionBackToPoolFunc,
+                    m_startPoolSize,
+                    m_maxPoolSize
+                );
+
+    return ObjectPool<Object>::ObjectPoolPtr(pool, [](ObjectPool<Object> *pool) { pool->destroy(); });
+}
+
+#endif // CONNECTIONPOOL_HPP
---
+++ tests.cpp
@@ -0,0 +1,136 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "objectpool.hpp"
+#include "gmock/gmock.h"
+
+using ::testing::AtLeast;
+
+namespace {
+
+    class EmptyObject {
+    public:
+        virtual ~EmptyObject() {};
+        virtual void closeFunc() = 0;
+        virtual void freeFunc() = 0;
+    };
+
+    class Object : public EmptyObject {
+    public:
+        MOCK_METHOD0(closeFunc, void());
+        MOCK_METHOD0(freeFunc, void());
+    };
+
+    class PoolTest : public ::testing::Test {
+    protected:
+
+        PoolTest() {
+        }
+
+        virtual ~PoolTest() {
+            if (m_obj != nullptr) {
+                delete m_obj;
+            }
+        }
+
+        virtual void SetUp() {
+        }
+
+        virtual void TearDown() {
+        }
+
+        Object *m_obj = nullptr;
+        void setObj(Object *obj) {
+            if (m_obj != nullptr) {
+                delete m_obj;
+            }
+        }
+    };
+
+    TEST_F(PoolTest, StartSize) {
+        int count = 0;
+        const int startSize = 3;
+
+        ObjectPool<int> pool(
+                [&count] () -> int* {
+                    count++;
+                    return nullptr;
+                },
+                [] (int *obj) {},
+                [] (int *obj) {},
+                startSize,
+                5
+            );
+
+        ASSERT_EQ(count, startSize);
+    }
+
+    TEST_F(PoolTest, Destructor) {
+        Object obj;
+        const int n = 8;
+
+        EXPECT_CALL(obj, freeFunc()).Times(n);
+        EXPECT_CALL(obj, closeFunc()).Times(0);
+
+        ObjectPool<Object> pool(
+                [&obj] { return &obj; },
+                [] (Object *obj) { obj->freeFunc(); },
+                [] (Object *obj) {},
+                n,
+                n
+        );
+
+    }
+
+    TEST_F(PoolTest, BackToPool) {
+        Object obj;
+        const int n = 8;
+
+        EXPECT_CALL(obj, freeFunc()).Times(n);
+        EXPECT_CALL(obj, closeFunc()).Times(n);
+
+        ObjectPool<Object> pool(
+                [&obj] { return &obj; },
+                [] (Object *obj) { obj->freeFunc(); },
+                [] (Object *obj) { obj->closeFunc(); },
+                n,
+                n
+        );
+
+        for (int i = 0; i < n; i++) {
+            auto ptr = pool.object();
+        }
+    }
+
+    TEST_F(PoolTest, NormalSize) {
+        setObj(new Object());
+        const int n = 8;
+
+        EXPECT_CALL(*m_obj, freeFunc()).Times(n - 1);
+        EXPECT_CALL(*m_obj, closeFunc()).Times(1);
+        Object *obj = m_obj;
+
+        auto *pool = new ObjectPool<Object>(
+                [obj] { return obj; },
+                [] (Object *obj) { obj->freeFunc(); },
+                [] (Object *obj) { obj->closeFunc(); },
+                1,
+                1
+        );
+
+        auto *ptrs = new std::vector<ObjectPool<Object>::ObjectPtr>(n);
+
+        for (int i = 0; i < n; i++) {
+            ptrs->push_back(pool->object());
+        }
+
+        delete ptrs;
+        //memmory leak
+    }
+}
+
+int main(int argc, char **argv) {
+    ::testing::GTEST_FLAG(throw_on_failure) = false;
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
